9247a6e962b28723e5500d49c1afbeaa1ce0d23c
AuTa <jintanboy@gmail.com>
2014-02-11 23:34:28 +0800

    only get bingwallpaper‘s url

--- src/bingwallpaper/bingwallpaper.py
+++ src/bingwallpaper/bingwallpaper.py
@@ -1,26 +1,29 @@
+# -*- coding: UTF-8 -*-
+
 import urllib.request
 import json
-import os
 
 BASE_URL = 'http://www.bing.com'
-url_json = urllib.request.urlopen(BASE_URL + '/HPImageArchive.aspx?format=js&idx=0&n=1&cc=us')
+url_json = urllib.request.urlopen(BASE_URL + '/HPImageArchive.aspx?format=js&idx=0&n=1&cc=cn')
 content = url_json.read().decode('utf-8')
 data = json.loads(content)
-
 images = data['images']
 urlbase = images[0]['urlbase']
 enddate = images[0]['enddate']
-if images[0]['msg']:
-    text = images[0]['msg'][0]['text']
-else:
-    text = urlbase.split('/')[4].split('_')[0]
 url = BASE_URL + urlbase + '_1920x1080.jpg'
 
-#保存到文件夹下，如果文件夹存在就跳过，如果不存在就新建文件夹
-if os.path.exists('BingWallpaper\\'):
-    pass
-else:
-    os.mkdir('BingWallpaper\\')
 
-with open('BingWallpaper\\' + enddate + '_' + text + '.jpg', 'wb') as a_file:
-    a_file.write(urllib.request.urlopen(url).read())
+#下载的图片名称
+def image_name():
+    if images[0]['msg']:
+        text = images[0]['msg'][0]['text']
+    else:
+        text = urlbase.split('/')[4].split('_')[0]
+    _image_name = '_'.join([enddate, text]) + '.jpg'
+    return _image_name
+
+
+#图片URL
+def image_url():
+    _image_url = BASE_URL + ''.join([urlbase + '_1920x1080.jpg'])
+    return _image_url
